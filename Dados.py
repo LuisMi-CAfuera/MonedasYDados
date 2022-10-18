602b4727fd15c1a5c21ef5513e3c2b6789034f7e
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2022-10-18 10:23:40 +0200

    Initial commit

---
+++ Dados.py
@@ -0,0 +1,38 @@
+import yaml
+from pprint import pprint
+import random
+
+d = { 'moneda': {
+    'cara':{},
+    'cruz': {}
+},
+      'dados':{}
+}
+
+moneda = ["cara","cruz"]
+def tirarmonedas():
+    return random.choice(moneda)
+
+def tiradaDado():
+    return random.randint(1, 6)
+
+if __name__ == "__main__":
+
+    for _ in range(1000000):
+        contCara=0
+        contCruz=0
+        dado=0
+        for _ in range(10):
+            if(tirarmonedas() == "cara"):
+                contCara=contCara+1
+            else:
+                contCruz=contCruz+1
+    
+        d['moneda']['cara'][contCara] += 1
+        d['moneda']['cruz'][contCruz] += 1
+        for _ in range(3):
+            dado += tiradaDado()
+        
+        d['dados'][dado] += 1
+
+    pprint(d)
